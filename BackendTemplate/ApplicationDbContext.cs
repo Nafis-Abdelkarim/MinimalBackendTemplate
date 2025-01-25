1410bf155b02ef4bb7b24df5176b4fa1b0442252
Nafis-Abdelkarim <kazcrb@gmail.com>
2025-01-25 22:06:45 +0100

    Add project files.

---
+++ BackendTemplate/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendTemplate
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {   
+        }
+
+        //public DbSet<Entity> EntitesName { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entity>(builder =>
+            {
+                //set Entity constraints and roles
+                //builder.HasIndex(s => s.Code).IsUnique();
+                //chek more att : https://learn.microsoft.com/en-us/ef/core/modeling/keys?tabs=data-annotations
+            });
+        }*/
+    }
+}
